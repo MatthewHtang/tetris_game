641f3d4d6e4fdd7655bbfeddeaaa4247179717f4
MatthewHtang <matthewhalingthang28@gmail.com>
2026-07-24 21:12:48 -0500

    Add initial game logic and structure for Tetris implementation

---
+++ game.py
@@ -0,0 +1,16 @@
+"""
+Import Dependecies
+"""
+import sys
+import pygame
+import random
+
+pygame.init()
+
+#Screen dimensions
+WIDTH, HEIGHT = 800, 600
+GRID_SIZE = 25
+
+#Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
