5d2b5abb64b50f335297f4bc725e52718aacf3f4
MatthewHtang <matthewhalingthang28@gmail.com>
2026-07-25 20:56:40 -0500

    Refactor game.py to define colors and add Tetromino figures with initial display logic

--- game.py
+++ game.py
@@ -15,3 +15,25 @@ GRID_SIZE = 25
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (128, 128, 128)
+
+colors = [(WHITE), (BLACK), (GRAY)]
+
+#Tetromino figures
+class Figure:
+    SCREEN_PLAY = r"""
+
+HHHHHHHHH   HHHHHHHHH   HHHHHHHHH   HHHHHHHHH   HHH   HHHHHHHHH
+   HHH      HHH            HHH      HHH   HHH   HHH   HHH
+   HHH      HHH            HHH      HHH   HHH   HHH   HHH
+   HHH      HHHHHH         HHH      HHHHHH      HHH   HHHHHHHHH
+   HHH      HHHHHH         HHH      HHHHHH      HHH   HHHHHHHHH
+   HHH      HHH            HHH      HHH   HHH   HHH         HHH
+   HHH      HHHHHHHHH      HHH      HHH   HHH   HHH   HHHHHHHHH
+   HHH      HHHHHHHHH      HHH      HHH   HHH   HHH   HHHHHHHHH
+
+   
+            P R E S S  A N Y  K E Y  T O  S T A R T
+"""
+    def __init__(self):
+        print(self.SCREEN_PLAY)
+Figure()
